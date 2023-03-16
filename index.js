374bd95a3fb183f1b731368e4c9be671a02e2883
Tyler Kirumba <Tyler>
2023-03-16 18:09:23 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity="roller-skate"){
+    return("This Saturday, I want to " + activity + "!")
+
+}
+
+function mondayWork(activity = "go to the office"){
+    return ('This Monday, I will ' + activity + "." )
+
+}
+
+
+
+function wrapAdjective(input="*") {
+    return function(compliment = 'special'){
+    return("You are " + input + compliment + input + "!");
+}
+  }
+
+
+  
+ 
